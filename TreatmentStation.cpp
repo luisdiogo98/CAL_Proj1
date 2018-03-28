f7e588ba61031d816a83a68f16a2136ba11db48c
Rekicho <up201604145@fe.up.pt>
2018-03-28 14:55:56 +0100

    Added TreatmentStation

---
+++ TreatmentStation.cpp
@@ -0,0 +1,5 @@
+#include "TreatmentStation.h"
+
+TreatmentStation::TreatmentStation()
+{
+}
---
+++ TreatmentStation.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "Truck.h"
+#include "Landmark.h"
+
+class TreatmentStation : public Landmark
+{
+public:
+	TreatmentStation();
+};
