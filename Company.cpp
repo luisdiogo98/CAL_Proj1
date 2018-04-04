0aef7a7a38b5813c0a3bca36365b5acfd8aecefe
luisdiogo98 <luisdiogo98@users.noreply.github.com>
2018-04-04 15:03:37 +0000

    Added company.cpp

---
+++ Company.cpp
@@ -0,0 +1,53 @@
+#include "..\..\CAL_Proj1\Company.h"
+
+Company::Company(Graph<Landmark*> m)
+{
+	map = m;
+}
+
+Graph<Landmark*> Company::getMap()
+{
+	return map;
+}
+
+vector<Landmark*> Company::getGarages()
+{
+	return Garages;
+}
+
+vector<Landmark*> Company::getTreatmentStations()
+{
+	return TreatmentStations;
+}
+
+vector<Landmark*> Company::getFullContainers()
+{
+	return FullContainers;
+}
+
+void Company::addGarage(Landmark * l)
+{
+	Garages.push_back(l);
+}
+
+void Company::addTreatmentStation(Landmark * l)
+{
+	TreatmentStations.push_back(l);
+}
+
+void Company::addFullContainer(Landmark * l)
+{
+	FullContainers.push_back(l);
+}
+
+void Company::removeFullContainer(Landmark * l)
+{
+	for (vector<Landmark*>::iterator it = FullContainers.begin(); it != FullContainers.end(); it++)
+	{
+		if (*it == l)
+		{
+			FullContainers.erase(it);
+			return;
+		}
+	}
+}
